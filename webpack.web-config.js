e37a8379ec751f0ab472b3b3ef5f3959e8f2819d
elbarbarosso510 <el_barbarosso510@protonmail.com>
2024-07-19 18:48:01 +0300

    Add files via upload

---
+++ webpack.web-config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/web/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist-web')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }, 
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'mdon'
+    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor'
+    // })
+  ],
+  devtool: 'cheap-module-eval-source-map'
+}
